21c46405a2e764478dc6f1033ff08258bed4ed90
Abhilash Pandurangan <abhilash@stylebot.app>
2021-04-18 08:08:37 -0700

    updated UI

--- run.py
+++ run.py
@@ -40,7 +40,7 @@ def tone_analyzer():
     from ibm_watson import ToneAnalyzerV3
     from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 
-    #authenticator = IAMAuthenticator('')
+    authenticator = IAMAuthenticator('')
     tone_analyzer = ToneAnalyzerV3(
         version='2017-09-21',
         authenticator=authenticator
